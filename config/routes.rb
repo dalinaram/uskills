0966aab552f6c437c8b6c5dcd1b474113dfbdce4
lajimi mahmoud <mahmoudladjimi@gmail.com>
2023-12-03 14:24:56 +0100

    add link skills

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,19 @@ Rails.application.routes.draw do
   root to: "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :skills do
+    resources :skills, only: [:create, :show, :edit, :update, :destroy]
     collection do
       get :search
     end
-    resources :skills, only: [:create, :show, :edit, :update, :destroy]
+
   end
 
   resources :users do
+
     resources :reviews, only: [:create, :show, :edit, :update, :destroy]
     resources :reservations
     resources :messages, only:[:create, :show]
+
   end
 
 end
