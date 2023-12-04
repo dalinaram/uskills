680fa37293ffa3a3714b6a92100820bcf01f3ed5
Dalina LESOURD <dalinaram@gmail.com>
2023-12-04 14:14:23 +0100

    cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_one_attached :photo
 end
