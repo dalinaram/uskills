30972ab2cfd71501e3d9ea30d2b482d85e355633
Thomas Traore <traore.thomas77@gmail.com>
2023-12-07 11:10:22 +0100

    done show

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :teacher, :class_name => 'User'
   belongs_to :student, :class_name => 'User'
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
